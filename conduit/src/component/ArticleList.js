f770f132da56d7296653625c6e96f6d22a832912
HastyAlvin <94995353+HastyAlvin@users.noreply.github.com>
2023-08-15 00:47:55 +0700

    Add files via upload

--- conduit/src/component/ArticleList.js
+++ conduit/src/component/ArticleList.js
@@ -1,38 +1,45 @@
-import ArticlePreview from './ArticlePreview';
-import ListPagination from './ListPagination';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ArticleList = props => {
-  if (!props.articles) {
-    return (
-      <div className="article-preview">Loading...</div>
-    );
-  }
+const ArticleList = () => {
+  const [articleList, setArticleList] = useState([]);
 
-  if (props.articles.length === 0) {
+  useEffect(() => {
+    // Gọi API để lấy danh sách bài viết
+    fetch('https://api.realworld.io/api/articles?limit=10&offset=0')
+      .then(response => response.json())
+      .then(data => {
+        setArticleList(data.articles);
+      })
+      .catch(error => {
+        console.error('Error fetching articles:', error);
+      });
+  }, []);
+
+  return articleList.map((article, index) => {
     return (
-      <div className="article-preview">
-        No articles are here... yet.
+      <div key={article.slug} class="article-preview">
+        <div class="article-meta">
+          <a href="profile.html">
+            <img src={article.author.image} />
+          </a>
+          <div class="info">
+            <a href="" class="author">
+              {article.author.username}
+            </a>
+            <span class="date"></span>
+          </div>
+          <button class="btn btn-outline-primary btn-sm pull-xs-right">
+            <i class="ion-heart"></i> 29
+          </button>
+        </div>
+        <a href="" class="preview-link">
+          <h1>{article.title}</h1>
+          <p>{article.description}</p>
+          <span>Read more...</span>
+        </a>
       </div>
     );
-  }
-
-  return (
-    <div>
-      {
-        props.articles.map(article => {
-          return (
-            <ArticlePreview article={article} key={article.slug} />
-          );
-        })
-      }
-
-      <ListPagination
-        pager={props.pager}
-        articlesCount={props.articlesCount}
-        currentPage={props.currentPage} />
-    </div>
-  );
+  });
 };
 
 export default ArticleList;
